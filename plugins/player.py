3856764871f27da8f2f7cceb0971ac9ab2dc22bd
akhacker <akhacker.program4hack@gmail.com>
2021-10-20 06:26:30 -0400

    update

--- plugins/player.py
+++ plugins/player.py
@@ -6,24 +6,47 @@ import time
 async def send_all(client, message):
   message_id = message.message_id
   chat_id = message.chat.id
-  get_inline = message.reply_markup.inline_keyboard
-  while True:
-    try:
-      for inline in get_inline:
-        # print(inline)
+  try:
+    get_inline = message.reply_markup.inline_keyboard
+  except Exception as error:
+    pass
+  timer = 427
+  try:
+    call_backs = []
+    i = 0
+    for inline in get_inline:
+      try:
         check_list = isinstance(inline, list)
         if check_list:
           for lis in inline:
-            time.sleep(3)
-            print("clicking " + str(lis.callback_data))
-            callback_data = lis.callback_data
-            a = await client.request_callback_answer(
-              chat_id=chat_id,
-              message_id=message_id,
-              callback_data=callback_data
-              )
-            print("Waiting for 7 minutes")
-            time.sleep(427)
-    except Exception as error:
-      print(str(error))
-      time.sleep(10)
+            if i <= 2:
+              call_backs.append(lis.callback_data)
+        else:
+          print(check_list)
+          # print(inline)
+      except Exception as error:
+        print(error)
+    print(call_backs)
+    for callback_data in call_backs:
+      try:
+        time.sleep(3)
+        print("clicking " + str(callback_data))
+        while True:
+          a = await client.request_callback_answer(
+                chat_id=chat_id,
+                message_id=message_id,
+                callback_data=callback_data,
+                timeout=5
+                )
+          if len(a) > 2:
+            print(a)
+            break
+          else:
+            time.sleep(10)
+            print("not getting response waiting for 10 sec")  
+        print("Waiting for "+ str(timer) +" seconds")
+        time.sleep(timer)
+      except Exception as error:
+        print(error)
+  except Exception as error:
+    print(error)
