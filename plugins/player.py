1a8652418bbc8ec096ecb699054305b3af05082f
akhacker <akhacker.program4hack@gmail.com>
2021-10-21 03:53:26 -0400

    time limite added

--- plugins/player.py
+++ plugins/player.py
@@ -7,12 +7,14 @@ async def send_all(client, message):
   message_id = message.message_id
   chat_id = message.chat.id
   time.sleep(2)
+  timer = 427
   if "🌲Forest " in message.text:
     for i in range(0,3):
       try:
         print("clicking " + str(i))
         await message.click(i)
-        time.sleep(10)
+        print("Waiting for " + str(timer))
+        time.sleep(427)
       except Exception as error:
         print(error)
     return
